180e661cf398f84053043efdd1b61ccfef7107a3
Álisson Melquíades <alisson@imaginationmedia.com>
2023-11-15 10:15:01 -0300

    Feature: Firebase config

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBv2DiGtqvusaw7727fBXlfwFi5KV77ZdY',
+    authDomain: 'temp-5bc33.firebaseapp.com',
+    databaseURL: 'https://temp-5bc33-default-rtdb.firebaseio.com',
+    projectId: 'temp-5bc33',
+    storageBucket: 'temp-5bc33.appspot.com',
+    messagingSenderId: '524940723425',
+    appId: '1:524940723425:web:df29dfb5d629935edfe3a0',
+    measurementId: 'G-02CS2QK7FR',
+  },
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,16 +1,13 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBv2DiGtqvusaw7727fBXlfwFi5KV77ZdY',
+    authDomain: 'temp-5bc33.firebaseapp.com',
+    databaseURL: 'https://temp-5bc33-default-rtdb.firebaseio.com',
+    projectId: 'temp-5bc33',
+    storageBucket: 'temp-5bc33.appspot.com',
+    messagingSenderId: '524940723425',
+    appId: '1:524940723425:web:df29dfb5d629935edfe3a0',
+    measurementId: 'G-02CS2QK7FR',
+  },
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
