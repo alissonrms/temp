872c938831bc93a524bf6edba86dc35b469d229a
Álisson Melquíades <alisson@imaginationmedia.com>
2023-11-15 10:11:56 -0300

    Feature: Auth guards

---
+++ frontend/src/app/guards/logged.guard.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoggedGuard {
+  constructor(private authService: AuthService, public router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    if (this.authService.isLoggedIn) {
+      this.router.navigate(['app']);
+      return false;
+    }
+
+    return true;
+  }
+}
