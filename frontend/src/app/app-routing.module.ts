c1e3f7e96239690d84318ff4898f94fcef4652fc
Álisson Melquíades <alisson@imaginationmedia.com>
2023-11-15 10:11:13 -0300

    Feature: App component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,24 +1,53 @@
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { AppLayoutComponent } from "./layout/app.layout.component";
+import { AppLayoutComponent } from './layout/app.layout.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { AuthGuard } from './guards/auth.guard';
+import { LoggedGuard } from './guards/logged.guard';
 
 @NgModule({
-    imports: [
-        RouterModule.forRoot([
+  imports: [
+    RouterModule.forRoot(
+      [
+        {
+          path: 'app',
+          component: AppLayoutComponent,
+          canActivate: [AuthGuard],
+          children: [
             {
-                path: 'app', component: AppLayoutComponent,
-                children: [
-                    { path: '', loadChildren: () => import('./modules/device/device.module').then(m => m.DeviceModule) }
-                ]
+              path: '',
+              loadChildren: () =>
+                import('./modules/device/device.module').then(
+                  (m) => m.DeviceModule
+                ),
             },
-            { path: 'auth', loadChildren: () => import('./modules/authentication/authentication.module').then(m => m.AuthenticationModule) },
-            { path: '', loadChildren: () => import('./modules/landing/landing.module').then(m => m.LandingModule) },
-            { path: 'notfound', component: NotFoundComponent },
-            { path: '**', redirectTo: '/notfound' },
-        ], { scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled', onSameUrlNavigation: 'reload' })
-    ],
-    exports: [RouterModule]
+          ],
+        },
+        {
+          path: 'auth',
+          canActivate: [LoggedGuard],
+          loadChildren: () =>
+            import('./modules/authentication/authentication.module').then(
+              (m) => m.AuthenticationModule
+            ),
+        },
+        {
+          path: '',
+          loadChildren: () =>
+            import('./modules/landing/landing.module').then(
+              (m) => m.LandingModule
+            ),
+        },
+        { path: 'notfound', component: NotFoundComponent },
+        { path: '**', redirectTo: '/notfound' },
+      ],
+      {
+        scrollPositionRestoration: 'enabled',
+        anchorScrolling: 'enabled',
+        onSameUrlNavigation: 'reload',
+      }
+    ),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,44 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppLayoutModule } from './layout/app.layout.module';
 import { MessageService } from 'primeng/api';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { environment } from 'src/environments/environment';
+import { AuthService } from './services/auth.service';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { UtilsService } from './services/utils.service';
+import { MessagesModule } from 'primeng/messages';
+import { ToastModule } from 'primeng/toast';
+
+import { registerLocaleData } from '@angular/common';
+import localeBr from '@angular/common/locales/pt';
+
+registerLocaleData(localeBr, 'pt')
 
 @NgModule({
-    declarations: [AppComponent, NotFoundComponent],
-    imports: [AppRoutingModule, AppLayoutModule],
-    providers: [
-        { provide: LocationStrategy, useClass: HashLocationStrategy },
-        MessageService,
-    ],
-    bootstrap: [AppComponent],
+  declarations: [AppComponent, NotFoundComponent],
+  imports: [
+    AppRoutingModule,
+    AppLayoutModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireDatabaseModule,
+    MessagesModule,
+    ToastModule,
+  ],
+  providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
+    { provide: LOCALE_ID, useValue: 'pt' },
+    MessageService,
+    AuthService,
+    UtilsService
+  ],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
